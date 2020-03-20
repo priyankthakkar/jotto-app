91896a59cdd356d6256cea20ac137912b5d6c40b
Priyank Thakkar <priyankthakkar@gmail.com>
2020-03-20 16:22:44 +1100

    Change Log: 1. Setting up redux for the project.

---
+++ src/reducers/index.js
@@ -0,0 +1,8 @@
+import {combineReducers} from "redux";
+import success from "./successReducer";
+
+const rootReducer = combineReducers({
+    success
+});
+
+export default rootReducer;
---
+++ src/reducers/successReducer.js
@@ -0,0 +1,17 @@
+const INITIAL_STATE = false;
+
+/**
+ * successReducer
+ *
+ * @name successReducer
+ * @function
+ * @param {boolean} state=INITIAL_STATE contains the current state of success.
+ * @param {object} action action to be reduced.
+ * @returns {boolean} new success state
+ */
+
+const successReducer = (state = INITIAL_STATE, action) => {
+    return state;
+};
+
+export default successReducer;
